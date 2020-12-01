0a0a5a5a4202bc1a3811b4ded400c8cacc7b1d56
Sebastian Skałacki <skalee@gmail.com>
2020-12-01 04:51:31 +0100

    Remove placeholder spec

--- spec/jekyll/pry_spec.rb
+++ spec/jekyll/pry_spec.rb
@@ -5,8 +5,4 @@ RSpec.describe Jekyll::Pry do
   it "has a version number" do
     expect(Jekyll::Pry::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
